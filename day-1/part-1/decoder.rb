e304692eb77866a151647c6333e6fc1bc0404bd5
rich-27 <rmlewis14@gmail.com>
2025-12-01 17:05:49 +0000

    Solve day-1

---
+++ day-1/part-1/decoder.rb
@@ -0,0 +1,19 @@
+dial_position = 50
+zero_count = 0
+
+input_data = File.read("../input.txt").split.map { |line| { direction: line[0], amount: line[1..].to_i } }
+
+for direction, amount in input_data.map { |operation| operation.values_at(:direction, :amount) }
+    case direction
+    when 'L'
+        dial_position = (dial_position - amount) % 100
+    when 'R'
+        dial_position = (dial_position + amount) % 100
+    end
+
+    zero_count += 1 if dial_position == 0
+
+    puts "Dial moved #{direction}#{amount} to position #{dial_position}"
+end
+
+puts "Password is: #{zero_count}"
