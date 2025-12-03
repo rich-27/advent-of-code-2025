648acf435b4112112cf831113a81f7f4df983bc3
rich-27 <rmlewis14@gmail.com>
2025-12-03 16:45:29 +0000

    Format day-3

--- day-3/part-2/controller.rb
+++ day-3/part-2/controller.rb
@@ -1,21 +1,21 @@
-battery_joltages = File.readlines("../input.txt", chomp: true).map { |line| line.split('').map(&:to_i) }
+battery_joltages = File.readlines('../input.txt', chomp: true).map { |line| line.split('').map(&:to_i) }
 
 powered_count = 12
 
 output_joltages = battery_joltages.map do |bank_joltages|
   candidates = bank_joltages.dup
 
-  for pick in (0...powered_count)
+  (0...powered_count).each do |pick|
     remaining_picks = powered_count - pick
     picked_joltage = candidates[pick..(candidates.length - remaining_picks)].max
     drop_count = candidates[pick..].take_while { |joltage| joltage < picked_joltage }.count
-    candidates.slice!(pick, drop_count) if drop_count > 0
+    candidates.slice!(pick, drop_count) if drop_count.positive?
   end
 
   candidates[0...powered_count].join('').to_i
 end
 
-for bank_joltages, output_joltage in battery_joltages.zip(output_joltages)
+battery_joltages.zip(output_joltages).each do |bank_joltages, output_joltage|
   puts "Bank #{bank_joltages.join('')} yields #{output_joltage}"
 end
 
