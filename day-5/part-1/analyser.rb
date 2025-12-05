308a15771ed799546c3788fbb5abefacd9484d07
rich-27 <rmlewis14@gmail.com>
2025-12-05 10:39:43 +0000

    Solve day-5 part-1

---
+++ day-5/part-1/analyser.rb
@@ -0,0 +1,16 @@
+def parse_input(fresh_id_ranges_string, available_ids_string)
+  [
+    fresh_id_ranges_string.split("\n").map do |range_string|
+      range_string.split('-').map(&:to_i).then do |range_start, range_end|
+        range_start..range_end
+      end
+    end,
+    available_ids_string.split("\n").map(&:to_i)
+  ]
+end
+
+fresh_id_ranges, available_ids = parse_input(*File.read('../input.txt').split("\n\n"))
+
+puts "Fresh id count: #{available_ids.select do |id|
+  fresh_id_ranges.any? { |fresh_id_range| fresh_id_range.cover?(id) }
+end.count}"
